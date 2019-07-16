690feea8d4d5d6525bb1d40f349db204ee93f1e7
Mitchell Dill <malexanderdill@gmail.com>
2019-07-16 16:52:59 -0500

    Reconfigure boilerplate to better suite proxy, set up basic server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(cors());
+
+app.use(express.static("dist"));
+
+app.listen(PORT, () => {'the proxy is up my friend'});
