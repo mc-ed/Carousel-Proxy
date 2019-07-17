e9fe0ec7577aedf5dc5c32707be3ccb6dc940484
Mitchell Dill <malexanderdill@gmail.com>
2019-07-17 10:44:33 -0500

    Create functioning proxy server for SOA using bootstrap

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
-const PORT = 3000;
+const PORT = 3050;
 
 app.use(express.json());
 app.use(express.urlencoded());
